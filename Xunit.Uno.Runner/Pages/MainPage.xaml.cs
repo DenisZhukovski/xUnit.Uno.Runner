60a115b57b591b01149106c1b87ea4670e31d16b
Dzianis Zhukouski <dzianis_zhukouski@epam.com>
2024-03-16 23:41:51 +0300

    Improve ListView items selection

--- Xunit.Uno.Runner/Pages/MainPage.xaml.cs
+++ Xunit.Uno.Runner/Pages/MainPage.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI.Xaml.Input;
 using Xunit.Uno.Runner.Navigation;
 
 namespace Xunit.Uno.Runner;
@@ -33,14 +34,5 @@ public sealed partial class MainPage : Page
         {
             navigationAware.OnNavigatedFrom(e);
         }
-    }
-
-    private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        if (e.AddedItems.Any())
-        {
-            (DataContext as MainViewModel).TestCasesCommand.Execute(e.AddedItems.First());
-        }
-        AllTestsList.SelectedItems.Clear();
     }
 }
